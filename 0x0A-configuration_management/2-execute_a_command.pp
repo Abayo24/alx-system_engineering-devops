d4c24694e833ba66d28f6f573b9f8d08df367bde
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-23 02:52:08 +0300

    exec

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,5 @@
-# Puppet manifest to kill a process named killmenow
-
-# Define the exec resource to kill the process
-exec { 'killmenow_process':
-  command     => 'pkill -f killmenow',
-  path        => ['/usr/bin', '/bin'],  # Add appropriate paths if needed
-  refreshonly => true,  # Run only when triggered
+# Using exec to kill a process named killmenow
+exec {'kill-killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin';
 }
