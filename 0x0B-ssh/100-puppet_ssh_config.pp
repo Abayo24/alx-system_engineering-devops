6192199d48702419977e705ea47943c41588a59b
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-24 23:44:46 +0300

    using puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,10 +3,10 @@
 
 file { '/etc/ssh/ssh_config':
 	ensure  => present,
-	content =>"
+	content =>'
 	  #SSH client config
 	  Host *
 	      IdntityFile ~/.ssh/school
 	      PasswordAuthentication no
-	",
+	',
 }
