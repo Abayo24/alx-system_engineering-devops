2c4ac32fdc0e88ccdb7b12bb167722f62a8ae5ee
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-04-03 02:48:26 +0300

    custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,53 @@
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
+# Puppet script to configure custom HTTP response header
+class nginx_custom_header {
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => "# Default server configuration\n
+                \n
+                server {\n
+                        listen 80 default_server;\n
+                        listen [::]:80 default_server;\n
+                \n
+                        # Add custom HTTP response header\n
+                        add_header X-Served-By $::hostname;\n
+                \n
+                        root /var/www/html;\n
+                \n
+                        index index.html index.htm index.nginx-debian.html;\n
+                \n
+                        server_name _;\n
+                \n
+                        location / {\n
+                                try_files $uri $uri/ =404;\n
+                        }\n
+                \n
+                        location = /404.html {\n
+                                root /usr/share/nginx/html;\n
+                        }\n
+                \n
+                        location ~ \\.php$ {\n
+                                include snippets/fastcgi-php.conf;\n
+                                fastcgi_pass unix:/run/php/php7.4-fpm.sock;\n
+                        }\n
+                \n
+                        location ~ /\\.ht {\n
+                                deny all;\n
+                        }\n
+                }\n",
+    notify  => Service['nginx'],
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/nginx/sites-available/default'],
+  }
 }
--> package {'nginx':
-  ensure => present,
-}
--> file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
-}
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
-}
+
+include nginx_custom_header
+
