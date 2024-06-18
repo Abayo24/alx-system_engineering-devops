36fd0ce5762f6cf3f66e35b72a21c89d10f961ca
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-06-18 14:32:53 +0300

    useragent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'custom user agent'}
+    headers = {'User-Agent': 'my-app/0.0.1'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
