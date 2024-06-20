d3c0ec327bac9d02de7283f5f64599406b8bd7e6
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-06-21 01:58:40 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""How many subs?"""
+"""
+How many subs
+"""
 
 import requests
 
@@ -9,15 +11,12 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'my-app/0.0.1'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
